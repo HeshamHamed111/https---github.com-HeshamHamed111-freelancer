847089c8cd996f3442bcb9864273789dac205e42
hesham <ehesham26@yahoo.com>
2022-05-29 20:36:08 +0200

    Text header

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,16 +1,16 @@
-import { DragHandleIcon, HamburgerIcon, LockIcon, PlusSquareIcon } from '@chakra-ui/icons'
+import { DragHandleIcon, LockIcon, PlusSquareIcon } from '@chakra-ui/icons'
 import { Box, Flex, Icon, Spacer, Text } from '@chakra-ui/react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faEllipsisVertical} from '@fortawesome/free-solid-svg-icons'
 import React from 'react'
 
-function Title() {
+function Title(props) {
   return ( 
     <Box bg="#ededed">
         <Flex minWidth='max-content' alignItems='center' gap='2'>
             <Text className="p-3 py-4" color='black' fontWeight={'bold'}>
             <Icon as={DragHandleIcon} color='gray' className="mr-2" />
-            Unit One 1
+              {props.text}            
             </Text>
             <Spacer />
             <Text>
